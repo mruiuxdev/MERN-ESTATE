360e60b7ea84d7212e097cf9ef13f8d8797a1871
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2025-11-17 14:08:12 +0200

    React router dom aded

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
-  return <div className="min-h-dvh"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<About />} path="/about" />
+        <Route element={<SignUp />} path="/sign-up" />
+        <Route element={<SignIn />} path="/sign-in" />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
