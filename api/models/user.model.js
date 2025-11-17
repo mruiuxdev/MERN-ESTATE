2003cc1f0ee7e74dcdefde564c0e578b15647c46
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2025-11-17 16:25:16 +0200

    Create user.model.js

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = new model("User", userSchema);
+
+export default User;
